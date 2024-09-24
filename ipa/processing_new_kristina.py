fbfe9ca9faef5e1f8c8dd86ccdc7de7540d9cc53
NessLfy <nessim.louafi@protonmail.com>
2024-09-24 18:00:42 +0200

    add processing scripts (spot detection)

--- ipa/processing_new_kristina.py
+++ ipa/processing_new_kristina.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-from multiprocessing import Pool
-from skimage.morphology import ball, dilation
-import ipa.src.snakemake_utils as snk
-import nd2
+from multiprocessing import Pool,Lock
+from skimage.morphology import ball
+import dask.array as da
 from skimage.morphology import disk,white_tophat
 from trackpy.preprocessing import lowpass
 from itertools import starmap
@@ -15,7 +14,11 @@ import argparse
 import tqdm
 import os
 from scipy.ndimage import maximum_filter
-import dask
+import dask.array as da
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import cProfile
+import nd2
+
 
 # function to filter the image
 
@@ -30,7 +33,7 @@ def max_filter(raw_im):
 
 # function to perform the detections
 
-def max5_detection(raw_im: np.ndarray,frame: int,channel:int,max_filter_image:np.array,labels:np.array,crop_size_xy:int = 4,crop_size_z:int = 4) -> pd.DataFrame:
+def max5_detection(raw_im: np.ndarray,frame: int,max_filter_image:np.array,labels:np.array,original_im:np.ndarray,crop_size_xy:int = 4,crop_size_z:int = 4) -> pd.DataFrame:
     """_summary_
 
     Args:
@@ -48,6 +51,7 @@ def max5_detection(raw_im: np.ndarray,frame: int,channel:int,max_filter_image:np
     n_pixels = []
     snr = []
     snr_d = []
+    snr_o = []
     for l in np.unique(labels):
         if l == 0:
             continue
@@ -55,17 +59,27 @@ def max5_detection(raw_im: np.ndarray,frame: int,channel:int,max_filter_image:np
             im_masked_max = max_filter_image * (labels == l)
 
             raw_im_masked_ = raw_im * (labels == l)
+            original_im_masked = original_im * (labels == l)
 
             raw_im_masked = raw_im_masked_*(raw_im_masked_ == im_masked_max)
 
             new_im_flat = raw_im_masked.flatten()
+            original_im_masked_flat = original_im_masked.flatten()
+
             flat_index = np.argsort(new_im_flat)[-5:]
             intensity = new_im_flat[flat_index]
+
+            intensity_o = original_im_masked_flat[flat_index]
+
             back_d = np.mean(raw_im_masked[raw_im_masked>0])
+
             back = np.mean(raw_im_masked_[raw_im_masked_>0])
+
+            back_or = np.mean(original_im_masked[original_im_masked>0])
 
             snr_d.extend([i/back_d for i in intensity])
             snr.extend([i/back for i in intensity])
+            snr_o.extend([i/back_or for i in intensity_o])
 
             max_coords_2.extend([np.unravel_index(f, raw_im_masked.shape) for f in flat_index])
 
@@ -88,115 +102,137 @@ def max5_detection(raw_im: np.ndarray,frame: int,channel:int,max_filter_image:np
     n_pixels = np.array(n_pixels)[~near_edge]
     snr = np.array(snr)[~near_edge]
     snr_d = np.array(snr_d)[~near_edge]
+    snr_o = np.array(snr_o)[~near_edge]
     z,y,x = pos.T
 
     
-    intensity = [raw_im[z,y,x] for (z,y,x) in zip(z,y,x)]
+    intensity = [original_im[z,y,x] for (z,y,x) in zip(z,y,x)]
 
 
     if len(x) == 0 or len(y) == 0 or len(z) == 0:
-        print(np.array(max_coords_2).T)
         print("No spots detected")
         return pd.DataFrame(columns=['x','y','z','x_fitted','y_fitted','z_fitted','frame','channel','intensity','pixel_sum','label','snr_tophat','snr_dilated'])
 
-    try:
-        k = [(raw_im,pos[i][1],pos[i][2],pos[i][0],crop_size_xy,crop_size_z) for i in range(len(pos))]
-    except IndexError as e:
-        print(f'The following error has occured when trying to create the arguments for the fitting {e}')
-        return pd.DataFrame(columns=['x','y','z','x_fitted','y_fitted','z_fitted','frame','channel','intensity','pixel_sum','label','snr_tophat','snr_dilated'])
-    
-    try:
-        x_s,y_s,z_s= zip(*(starmap(locate_com,k)))
-    except IndexError as e:
-        print(f'An error occurred when trying to map the locate_com function: {e}')
-        return pd.DataFrame(columns=['x','y','z','x_fitted','y_fitted','z_fitted','frame','channel','intensity','pixel_sum','label','snr_tophat','snr_dilated'])
-        
-    df_loc = pd.DataFrame([x,y,z,x_s,y_s,z_s]).T
-    df_loc.rename(columns={0:'x',1:'y',2:'z',3:'x_fitted',4:'y_fitted',5:'z_fitted'},inplace=True)
+    df_loc = pd.DataFrame([x,y,z]).T
+    df_loc.columns = ['x','y','z']
+
 
     try:
         detections_refined= tp.refine.refine_com(raw_image = raw_im, image= raw_im, radius= [crop_size_z//2,crop_size_xy//2,crop_size_xy//2], coords = df_loc, max_iterations=1,
         engine='python', shift_thresh=0.6, characterize=True,
-        pos_columns=['z_fitted','y_fitted','x_fitted'])
+        pos_columns=['z','y','x'])
     except ValueError:
         detections_refined = df_loc
 
-    df_loc[['z_fitted_refined','x_fitted_refined','y_fitted_refined']] = detections_refined[['z_fitted','x_fitted','y_fitted']]
+    df_loc[['z_fitted_refined','x_fitted_refined','y_fitted_refined']] = detections_refined[['z','y','x']]
 
     df_loc['frame'] = frame
-    df_loc['channel'] = channel
     df_loc['intensity'] = intensity
     df_loc['pixel_sum'] = n_pixels
     df_loc['label'] = labs
     df_loc['snr_tophat'] = snr
     df_loc['snr_dilated'] = snr_d
+    df_loc['snr_original'] = snr_o
 
     return df_loc
 
-
 # function to load and save the data
 
-def processing(input_path,output_file_path,frame,channel,labels,crop_xy,crop_z):
-    if os.path.exists(output_file_path):
-        return None
-    else:
-        im = nd2.imread(input_path,dask=True)
-        im = im[frame,:,channel,...].compute()
-        im_filtered = format(im)
-        max_filter_image = max_filter(im_filtered)
+def processing(input_path, output_file_path, frame, labels, crop_xy, crop_z):
+    with lock:
+        # Check if the output file already exists
+        if os.path.exists(output_file_path):
+            return None
+    # Proceed with processing outside the lock to allow parallel computation
+    im = nd2.imread(input_path, dask=True)
+    labels = da.from_zarr(labels, component='0/')
 
-        spot_df = max5_detection(raw_im= im_filtered,frame= frame,channel=channel,max_filter_image=max_filter_image,labels=labels,crop_size_xy= crop_xy,crop_size_z= crop_z)
+    im = im[frame,:,...].compute()
+    labels = labels[frame,...].compute()
 
-        spot_df.to_csv(output_file_path,index=False)
+    im_filtered = format(im)  # Assuming format is a predefined function
+    max_filter_image = max_filter(im_filtered)  # Assuming max_filter is a predefined function
 
-# function to perform the detections
+    # Assuming max5_detection is a predefined function that returns a DataFrame
+    spot_df = max5_detection(raw_im=im_filtered, frame=frame, max_filter_image=max_filter_image, labels=labels, crop_size_xy=crop_xy, crop_size_z=crop_z,original_im=im)
+
+    # Use the lock again for writing the file to ensure thread-safe I/O
+    with lock:
+        spot_df.to_csv(output_file_path, index=False)
+
+def init_process(shared_lock):
+    global lock
+    lock = shared_lock
+
+def processing_chunk(chunk):
+    results = []
+    for args in chunk:
+        # Unpack the arguments
+        input_path, output_file_path, frame, labels, crop_xy, crop_z = args
+        # Process each item in the chunk
+        result = processing(input_path, output_file_path, frame, labels, crop_xy, crop_z)
+        results.append(result)
+    return results
+
+def chunkify(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def read_and_concatenate_csv(files):
+    df_temp = pd.read_csv(files)
+    return df_temp
 
 def main():
+    lock = Lock()
     # Create the parser
     parser = argparse.ArgumentParser(description='Run detections')
 
     # Add the arguments
     parser.add_argument('--input_image', type=str, help='The input image file')
     parser.add_argument('--output_file', type=str, help='The output file')
-    parser.add_argument('--log_filename', type=str, help='The log filename')
-    parser.add_argument('--log_path', type=str, help='The log path')
     parser.add_argument('--threads', type=int, help='The number of threads')
     parser.add_argument('--labels', type=str, help='label_image')
-    parser.add_argument('--n_frames', type=int, help='frame')
     parser.add_argument('--crop_size_xy', type=int, help='crop_size_xy')
     parser.add_argument('--crop_size_z', type=int, help='crop_size_z')
 
     args = parser.parse_args()
 
     input_path = args.input_image
-    im = nd2.imread(input_path,dask=True)
     # dask.config.set(scheduler='threads', num_workers=1)
 
-    output_path = (args.output_file).strip('.csv') 
     threads = args.threads
-    labels = np.load(args.labels)
-    n_frames = np.shape(im)[0]
+    
+    im = nd2.imread(input_path, dask=True)
 
-    k = [(input_path,f"{output_path}_{frame}_{channel}.csv",frame,channel,labels[frame,...],args.crop_size_xy,args.crop_size_z) for frame in range(n_frames) for channel in range(2)]
+    output_path = (args.output_file).strip('.csv') 
+    
+    labels = args.labels
 
-    with Pool(threads) as p:
-        p.starmap(processing,tqdm.tqdm(k,total=len(k)))
+    n_frames = im.shape[0]
 
-    df_final = []
-    for files in tqdm.tqdm(os.listdir('/'.join(output_path.split('/')[:-1]))):
-        if files.endswith('.csv'):
-            if output_path.split('/')[-1] in files:
-                try:
-                    df_temp = pd.read_csv('/'.join(output_path.split('/')[:-1])+'/'+files)
-                    df_final.append(df_temp)
-                    os.remove('/'.join(output_path.split('/')[:-1])+'/'+files)
-                except UnicodeDecodeError:
-                    print(f'Error reading {files}')
-                    continue
+    tasks = [
+    (input_path,f"{output_path}_{frame}.csv", frame, labels, args.crop_size_xy, args.crop_size_z)
+    for frame in range(n_frames)]
 
-    df_final = pd.concat(df_final)
+    chunk_size =max(1, len(tasks) // (threads * 3))
 
-    df_final.to_parquet(output_path+'.csv',index=False)
+    with Pool(initializer=init_process, initargs=(lock,),processes=threads) as pool:
+        task_chunks = list(chunkify(tasks, chunk_size))
+        _ = list(tqdm.tqdm(pool.imap(processing_chunk, task_chunks), total=len(task_chunks)))
+
+    # Parallel file processing
+    files_to_process = [os.path.join('/'.join(output_path.split('/')[:-1]), f) for f in os.listdir('/'.join(output_path.split('/')[:-1])) if f.endswith('.csv') and output_path.split('/')[-1] in f and 'wrong_intensity' not in f]
+    with ProcessPoolExecutor(max_workers=threads) as executor:
+        df_futures = [executor.submit(read_and_concatenate_csv, file) for file in files_to_process]
+    
+    df_final = pd.concat([future.result() for future in tqdm.tqdm(as_completed(df_futures), total=len(df_futures))])
+    
+    # Clean up files
+    for file in files_to_process:
+        os.remove(file)
+    
+    df_final.to_parquet(output_path+'.csv', index=False)
 
 if __name__ == '__main__':
     main()
